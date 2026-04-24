532b6c31a2f2e9129a1b8cb645862c74388a28d5
Marc Gavanier <marc@gavanier.com>
2026-04-24 22:32:38 +0200

    feat: separate client and server entry points

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export * from './blocks';
-export * from './headless';
 export * from './icons';
 export * from './primitives';
-export * from './theme';
 export * from './utils';
---
+++ src/primitives-client/index.ts
@@ -0,0 +1,4 @@
+export * from '../primitives/dropdown';
+export * from '../primitives/options';
+export * from '../primitives/popover';
+export * from '../primitives/toggle-state';
