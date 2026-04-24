cccf059311d5e98da5dd4339f2b16896b847a3f5
Marc Gavanier <marc@gavanier.com>
2026-04-24 23:05:50 +0200

    fix: move combobox, modal, sortable-list to primitives-client

--- src/primitives-client/index.ts
+++ src/primitives-client/index.ts
@@ -2,3 +2,6 @@ export * from '../primitives/dropdown';
 export * from '../primitives/options';
 export * from '../primitives/popover';
 export * from '../primitives/toggle-state';
+export * from '../primitives/combobox';
+export * from '../primitives/modal';
+export * from '../primitives/sortable-list';
